64da9ff444e3d55a0fa77eed824d837d883e21e2
Valerio Podrini <valerio.podrini@gmail.com>
2020-06-24 15:24:16 +0200

    Revert "Revert "Revert "Revert "REST FRAMEWORK 20200623""""
    This reverts commit 5d092c7a24f58207a33fd0420ea2192b1b0cb0c0.

--- eventi/urls.py
+++ eventi/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls import url
 from django.urls import path, re_path, include
 from rest_framework.routers import DefaultRouter
 from .ajax_views import RelatedImageAJAXView
-from .views import EventiImageList
+from .views import EventiImageList, MyUploadView, FileUploadView
 
 ajax_router = DefaultRouter()
 ajax_router.register(r'', RelatedImageAJAXView)
 
+
 urlpatterns = [
     url(r'^related_images/', include(ajax_router.urls)),
     url(r'api/', EventiImageList.as_view()),
+    path('upload/', MyUploadView.as_view()),
+    path('myupload/', FileUploadView.as_view())
 ]
